83f1f06e80bde7d03f14deea18289a683712d2ed
Jannah June Nieves <jannahjune@gmail.com>
2019-08-30 14:38:39 -0700

    BIT code bloat removed and spotify function added

--- liri.js
+++ liri.js
@@ -3,7 +3,10 @@ require("dotenv").config();
 var axios = require("axios");
 var fs = require("fs");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// var Spotify = require("note-spotify-api");
+// var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+moment().format("MMM Do YY");
 
 var searchType = process.argv[2];
 var searchTerm = process.argv[3];
@@ -23,7 +26,7 @@ function userSearch(searchType, searchTerm) {
             if (searchTerm) {
                 showSongInfo(searchTerm);
             }else {
-                showSongInfo("All the Small Things");
+                showSongInfo("The Sign");
             }
             break;
         case "movie-this":
@@ -44,42 +47,70 @@ function userSearch(searchType, searchTerm) {
 // BANDS IN TOWN - USED FOR CONCERT INFO
 function showConcertInfo(searchTerm) {
     axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp")
-    .then(function (err, response) {
-        console.log(response.data);
-        if (err) {
-            console.log("ERROR");
-        }else {
-            var concerts = JSON.parse(body);
-            for (var i = 0; i < concerts.length; i++) {
+    .then(function (response) {
+        // console.log(response.data);
+            for (var i = 0; i < response.data.length; i++) {
+                var datetime = response.data[i].datetime;
+                var dateArray = datetime.split("T");
+                
+
                 console.log("-------------CONCERT INFO-------------");
                 fs.appendFileSync("log.txt", "-------------CONCERT INFO-------------");
 
-                console.log(i);
-                fs.appendFileSync("log.txt", i+"\n");
 
-                console.log("Name of the Venue: " + concerts[i].venue.name);
-                fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name);
+                var concertInfo = 
+                    "\nName of Venue: " + response.data[i].venue.name +
+                    "\nVenue Location: " + response.data[i].venue.city + "," + response.data[i].venue.region +
+                    "\nDate of the Event: " + moment(dateArray[0], "MMM Do YY");
+                    // "\nDate of the Event: " + response.data[i].datetime;
+                console.log(concertInfo);
+                fs.appendFileSync("log.txt", concertInfo);
+            
+                // console.log(i);
+                // fs.appendFileSync("log.txt", i+"\n");
 
-                console.log("Venue Location: " + concerts[i].venue.name + "," + concerts[i].venue.region);
-                fs.appendFileSync("log.txt", "Venue Location: " + "concerts[i].venue.city" + "," + concerts[i].venue.region);
+                // console.log("Name of the Venue: " + response.data[i].venue.name);
+                // fs.appendFileSync("log.txt", "Name of the Venue: " + response.data[i].venue.name);
+
+                // console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+                // fs.appendFileSync("log.txt", "Venue Location: " + "response.data[i].venue.city" + ", " + response.data[i].venue.region);
 
                 // FORMAT WITH MOMENT
-                console.log("Date of Event: " + concerts[i].datetime);
-                fs.appendFileSync("log.txt", "Date of Event: " + concerts[i].datetime);
+                // console.log("Date of Event: " + moment(dateArr[0], "MM-DD-YYYY"));
+                // fs.appendFileSync("log.txt", "Date of Event: " + moment(dateArr[0], "MM-DD-YYYY"));
             }
-        }
-    }) 
-    
+        }).catch(function(err) {
+            console.log(err);
+        });
 }
 
 // SPOTIFY - USED FOR SONG INFO
 function showSongInfo(searchTerm) {
+    spotify.search({ 
+        type: "track", 
+        query: searchTerm 
+    }).then(function(response) {
+        for (var i = 0; i < 5; i++) {
+            
+            console.log("-------------CONCERT INFO-------------");
+                fs.appendFileSync("log.txt", "-------------CONCERT INFO-------------");
+
+            var spotifyInfo = 
+                "\nArtist(s): " + response.tracks.items[i].artists[0].name +
+                "\nSong Name: " + response.tracks.items[i].name +
+                "\nAlbum Name: " + response.tracks.items[i].album.name +
+                "\nPreview Link: " + response.tracks.items[i].preview_url;
+                console.log(spotifyInfo);
+        }
+    }).catch(function(err) {
+        console.log(err);
+    })
 
 }
 
 // OMDB - USED FOR MOVIE INFO
-function showMovieInfo(searchTerm) {
+// function showMovieInfo(searchTerm) {
 
-}
+// }
 
 // DEFAULT SEARCH WITH PARAMETERS FROM random.txt
