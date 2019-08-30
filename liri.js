7aa93869c6567e85f29b45355ecd511b9aa97c2f
Jannah June Nieves <jannahjune@gmail.com>
2019-08-29 20:26:31 -0700

    main variables and switch case coded

--- liri.js
+++ liri.js
@@ -1,6 +1,35 @@
 require("dotenv").config();
 
+var fs = require("fs");
 var keys = require("./keys.js");
-
 var spotify = new Spotify(keys.spotify);
 
+var searchType = process.argv[2];
+var searchTerm = process.argv[3];
+
+userSearch(searchType, searchTerm);
+
+function userSearch(searchType, searchTerm) {
+    switch (searchType) {
+        case "concert-this":
+            showConcertInfo(searchTerm);
+            break;
+        case "spotify-this-song":
+            showSongInfo(searchTerm);
+            break;
+        case "movie-this":
+            showMovieInfo(searchTerm);
+            break;
+        case "do-what-it-says":
+            showRandomTxtInfo();
+            break;
+        default: 
+            console.log("You have entered an incorrect search type. Please choose from one of the following: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says");
+    }
+}
+
+// BANDS IN TOWN - USED FOR CONCERT INFO
+
+// SPOTIFY - USED FOR SONG INFO
+
+// OMDB - USED FOR MOVIE INFO
